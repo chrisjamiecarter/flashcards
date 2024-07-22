a22d1401e93f83d9a3b4b0ef73f765a841ccba10
Chris Carter <108958713+cjc-sweatbox@users.noreply.github.com>
2024-07-22 12:04:30 +0100

    Implemented controllers and dtos
    #11

---
+++ src/Flashcards/Controllers/StudySessionController.cs
@@ -0,0 +1,34 @@
+﻿using Flashcards.Data.Managers;
+using Flashcards.Models;
+
+namespace Flashcards.Controllers;
+
+public class StudySessionController
+{
+    #region Fields
+
+    private readonly SqlDataManager _dataManager;
+
+    #endregion
+    #region Constructors
+    
+    public StudySessionController(SqlDataManager dataManager)
+    {
+        _dataManager = dataManager;
+    }
+
+    #endregion
+    #region Methods
+
+    public void AddStudySession(StudySessionDto studySession)
+    {
+        _dataManager.AddStudySession(studySession.StackId, studySession.DateTime, studySession.Score);
+    }
+
+    public IReadOnlyList<StudySessionDto> GetStudySessions()
+    {
+        return _dataManager.GetStudySessions().Select(x => new StudySessionDto(x)).ToList();
+    }
+
+    #endregion
+}
